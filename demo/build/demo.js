7367ad902141c6baf26b2092dd1f3a459d8ceb07
ajainarayanan <ajainarayanan@gmail.com>
2014-11-15 17:56:39 -0800

    Fixes the new words/string in the middle of play

--- demo/build/demo.js
+++ demo/build/demo.js
@@ -269,6 +269,7 @@ function demo() {
   c1.setDomain("Mississippi");
   $("button[data-domain-submit='crunchButton']").on("click", function() {
     c1.reset();
+    eventsArray = [];
     c1.setDomain($("textarea[data-text-area='inputDomain']").val());
   });
 
